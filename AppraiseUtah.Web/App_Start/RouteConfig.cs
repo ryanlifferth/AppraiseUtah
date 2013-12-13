fc0bec1d8323e8d2fede82d0745eaae5bb515bab
Ryan Lifferth <ryanlifferth@gmail.com>
2013-12-13 11:53:47 -0700

    Added more styling

--- AppraiseUtah.Web/App_Start/RouteConfig.cs
+++ AppraiseUtah.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace AppraiseUtah.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Appraisal", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "Appraisal", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
