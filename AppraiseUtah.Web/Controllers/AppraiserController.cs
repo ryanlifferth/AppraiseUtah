491c0f34edc131ee31701b36b5628f3aa291974b
Ryan Lifferth <ryanlifferth@gmail.com>
2013-12-16 17:23:49 -0700

    Additional formatting for app list

---
+++ AppraiseUtah.Web/Controllers/AppraiserController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppraiseUtah.Models;
+using AppraiseUtah.ServiceModels;
+
+namespace AppraiseUtah.Web.Controllers
+{
+    public class AppraiserController : Controller
+    {
+
+        #region Fields
+
+        AppraisalServiceModel _appraisalServiceModel = new AppraisalServiceModel();
+
+        #endregion
+
+
+        //
+        // GET: /Appraiser/
+
+        public ActionResult Index()
+        {
+            List<Appraiser> appraisers = _appraisalServiceModel.Get_Appraisers();
+
+            return View(appraisers);
+        }
+
+
+        //
+        // GET: /Appraiser/Find
+
+        public ActionResult Find(int id = 0)
+        {
+            Appraiser appraiserModel = new Appraiser();
+
+            if (id != 0)
+            {
+                appraiserModel = _appraisalServiceModel.Get_Appraiser(id);
+            }
+
+            return View(appraiserModel);
+        }
+
+
+
+
+        [ChildActionOnly]
+        public ActionResult _AppraiserDropdown()
+        {
+            //List<Appraiser> appraisers = new List<Appraiser>();
+            var appraisers = _appraisalServiceModel.Get_Appraisers();
+
+            return PartialView(appraisers);
+        }
+
+
+
+    }
+}
---
+++ AppraiseUtah.Web/Scripts/Common/Appraisers.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(
+    function () {
+
+        
+        // TODO:  Use knockoutJS
+        $("#appraiserList > .dropdown-menu > a").on('click', function () {
+            var content = $(this).html() + '&nbsp;<span class="caret"></span>';
+
+            $(this).parents("#appraiserList").children(".dropdown-selected").html(content);
+        });
+
+
+
+
+});
