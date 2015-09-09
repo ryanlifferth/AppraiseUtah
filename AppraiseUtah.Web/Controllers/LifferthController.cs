9f236b6bd6f936daf3fc1bfae2bc9267bc228872
Ryan Lifferth <ryanlifferth@gmail.com>
2015-09-09 15:01:14 -0600

    A few more updates

---
+++ AppraiseUtah.Web/Controllers/LifferthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppraiseUtah.Web.Controllers
+{
+    public class LifferthController : Controller
+    {
+        // GET: Lifferth
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
